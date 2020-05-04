d93d5a6c0903134e6780ba64e33d62f90558df16
Narendran Thangarajan <nart@google.com>
2020-05-04 09:45:41 -0700

    Added backward pass and corresponding tests. Yet to fix dying relu.

--- jaxnn/jaxnn.py
+++ jaxnn/jaxnn.py
@@ -5,15 +5,29 @@
 # The above command will take the input tensor, run 10 passes on a 2x2 FC
 # neural network optimizing for a binary classification on the labels given.
 import jax.numpy as np
+from jax import grad, jit, vmap, ops
 
 class Neuron(object):
-    weights = [];
-    bias = 0;
+    inputs = []
+    weights = []
+    bias = 0
+    learning_rate = 0.1 # Toy value. Real values are like 1e-6
 
     def __init__(self, num_inputs):
         self.weights = np.ones(num_inputs)
 
     def forward(self, inputs):
+        self.inputs = inputs # store these for backward pass
         linear = np.dot(self.weights, inputs) + self.bias
         relu = lambda x: x if x > 1 else 0
         return relu(linear)
+
+    def backward(self, input_loss_gradients):
+        sum_loss_gradients = np.sum(input_loss_gradients) # This is dL/dq
+        local_derivatives = grad(self.forward)(self.inputs) # can be computed and cached during forward pass. This is the dq/dx, dq/dy, etc.
+        global_loss_derivatives = sum_loss_gradients * local_derivatives # Chain rule for global derivative.
+
+        for i, weight in enumerate(self.weights): # Update weights
+            self.weights = ops.index_update(self.weights, i, weight - (self.learning_rate * global_loss_derivatives[i])) # SGD optimizer
+
+        return global_loss_derivatives
--- jaxnn/jaxnn_test.py
+++ jaxnn/jaxnn_test.py
@@ -10,15 +10,52 @@ class TestNeuron(unittest.TestCase):
 
     def test_default_init(self):
         n = Neuron(3)
-        np_testing.assert_array_equal(n.weights, np.array([1., 1., 1.]))
 
-        self.assertEqual(n.bias, 0)
+        expected_weights = np.array([1., 1., 1.])
+        expected_bias = 0
+
+        np_testing.assert_array_equal(n.weights, expected_weights)
+        self.assertEqual(n.bias, expected_bias)
 
     def test_forward_pass(self):
         inputs = np.array([10., 20., 30.])
         n = Neuron(len(inputs))
 
-        np_testing.assert_array_equal(n.forward(inputs), np.array(60.))
+        expected_output = np.array(60.)
+
+        np_testing.assert_array_equal(n.forward(inputs), expected_output)
+
+    def test_backward_pass_one_loss(self):
+        inputs = np.array([10., 20., 30.])
+        n = Neuron(len(inputs))
+
+        n.weights = np.array([3., 4., 5.])
+        n.forward(inputs)
+
+        backward_loss_gradients = np.array([2.0])
+        n.learning_rate = 0.1
+
+        expected_output = np.array([6., 8., 10.])
+        expected_weights = np.array([2.4, 3.2, 4.])
+
+        np_testing.assert_array_equal(n.backward(backward_loss_gradients), expected_output)
+        np_testing.assert_array_equal(n.weights, expected_weights)
+
+    def test_training_flow_second_input(self):
+        inputs = np.array([10., 2., 5.])
+        n = Neuron(len(inputs))
+        n.weights = np.array([1., 1., -1.])
+        n.learning_rate = 0.1
+
+        for i in range(20):
+            output = float(n.forward(inputs))
+            print("Output: ", output)
+
+            loss = np.array([output - inputs[1]]) # The learnt function is a selector, which selects second input.
+            print("Loss: ", loss)
+
+            n.backward(loss)
+            print("Weights: ", n.weights)
 
 if __name__ == '__main__':
     unittest.main()
